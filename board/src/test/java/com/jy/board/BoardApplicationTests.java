00d51ca0104b7dc2cd826382b0331e73f1197256
JongYoung <whddud102@naver.com>
2020-05-16 00:48:55 +0900

    테스트 클래스 추가
    BoardApplicationTests.java
    - DB 연결 테스트 클래스 추가

--- board/src/test/java/com/jy/board/BoardApplicationTests.java
+++ board/src/test/java/com/jy/board/BoardApplicationTests.java
@@ -1,13 +1,30 @@
 package com.jy.board;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BoardApplicationTests {
+public class BoardApplicationTests {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void testSqlSession() {
+		log.info("Sql Session : " + sqlSession);
 	}
 
 }
