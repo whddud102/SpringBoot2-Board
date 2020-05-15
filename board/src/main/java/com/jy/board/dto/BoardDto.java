95f3f5d0de4bd3ab7c711506e0518b539b5994b3
JongYoung <whddud102@naver.com>
2020-05-16 02:29:59 +0900

    클래스 추가
    BoardDto.java
    - 게시판 데이터를 전달할 DTO 클래스

---
+++ board/src/main/java/com/jy/board/dto/BoardDto.java
@@ -0,0 +1,21 @@
+package com.jy.board.dto;
+
+import java.time.LocalDateTime;
+
+
+import lombok.Data;
+
+@Data
+public class BoardDto {
+	private int boardIdx;
+	private String title;
+	private String content;
+	private int hitCnt;
+	private String creatorId;
+	private LocalDateTime createdDateTime;
+	private String updaterId;
+	private LocalDateTime updatedDateTime;
+	
+	
+	
+}
