aeba0ec045c77f87cfc015319e20e31d2e3f6a5b
JongYoung <whddud102@naver.com>
2020-05-18 19:37:23 +0900

    파일 처리 객체를 빈으로 등록
    WebMvcConfiguration.java
    - 파일 처리 관련 메소드를 제공해주는 객체 CommonsMultipartResolver를 빈으로 등록

--- board/src/main/java/com/jy/board/configuration/WebMvcConfiguration.java
+++ board/src/main/java/com/jy/board/configuration/WebMvcConfiguration.java
@@ -1,6 +1,8 @@
 package com.jy.board.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggerInterceptor());
+	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8");
+		multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);
+		return multipartResolver;
 	}
 }
