a77acd6a39270f051321fcfe09670bb4fea9415a
JongYoung <whddud102@naver.com>
2020-05-18 19:36:19 +0900

    자동 설정 수정
    BoardApplication.java
    - 파일 관련 자동 설정 클래스인 MultipartAutoConfiguration을 자동 설정에서 제외하도록 수정

--- board/src/main/java/com/jy/board/BoardApplication.java
+++ board/src/main/java/com/jy/board/BoardApplication.java
@@ -2,8 +2,10 @@ package com.jy.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+// 파일 관련 설정 자동 구성을 제외 시킴, 내가 등록한 CommonsMultiPartResolver를 사용할 것이기 때문
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class BoardApplication {
 
 	public static void main(String[] args) {
