e4f7f9653dbc220c5eae754bd9b97c492f5b597b
JongYoung <whddud102@naver.com>
2020-05-21 15:47:49 +0900

    FileUtils 기능 추가
    FileUtils.java
    - JPA 용 첨부파일 정보 조회 메서드 추가

--- board/src/main/java/com/jy/board/common/FileUtils.java
+++ board/src/main/java/com/jy/board/common/FileUtils.java
@@ -13,11 +13,15 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.jy.board.dto.AttachFileDto;
+import com.jy.board.entitiy.AttachFileEntity;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class FileUtils {
 	
-	public List<AttachFileDto> parseFileInfo(int boardIdx, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
+public List<AttachFileDto> parseFileInfo(int boardIdx, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
 		
 		// 파일 업로드 요청이 없으면 null 반환
 		if(ObjectUtils.isEmpty(multipartHttpServletRequest)) {
@@ -71,6 +75,75 @@ public class FileUtils {
 					
 					file = new File(folderPath + "/" + newFileName);
 					multipartFile.transferTo(file);
+				}
+			}
+		}
+		return fileList;
+	}
+	
+	
+	// oneToMany 어노테이션으로 인해서 첨부파일 클래스에 게시글 번호를 따로 저장할 필요가 없어짐
+	// 파라미터로 게시글 번호를 전달 받지 않도록 수정
+	public List<AttachFileEntity> parseFileInfo(MultipartHttpServletRequest multipartHttpServletRequest) {
+
+		// 파일 업로드 요청이 없으면 null 반환
+		if (ObjectUtils.isEmpty(multipartHttpServletRequest)) {
+			return null;
+		}
+
+		List<AttachFileEntity> fileList = new ArrayList<>();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+		ZonedDateTime current = ZonedDateTime.now(); // 현재 날짜 객체
+		String folderPath = "upload/" + current.format(formatter); // 오늘 날짜를 이용해서 폴더를 생성
+		File file = new File(folderPath);
+
+		if (!file.exists()) {
+			file.mkdirs(); // 하위 폴더까지 생성
+		}
+
+		// 파일 태그의 Name 속성들의 리스트를 가져옴
+		Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+
+		String newFileName, originalFileExtension, contentType;
+
+		while (iterator.hasNext()) {
+			List<MultipartFile> list = multipartHttpServletRequest.getFiles(iterator.next());
+
+			for (MultipartFile multipartFile : list) {
+				if (!multipartFile.isEmpty()) {
+					try {
+
+						contentType = multipartFile.getContentType();
+
+						if (ObjectUtils.isEmpty(contentType)) {
+							break;
+						} else {
+							if (contentType.contains("image/jpeg")) {
+								originalFileExtension = ".jpg";
+							} else if (contentType.contains("image/png")) {
+								originalFileExtension = ".png";
+							} else if (contentType.contains("image/gif")) {
+								originalFileExtension = ".gif";
+							} else {
+								break;
+							}
+						}
+
+						newFileName = Long.toString(System.nanoTime()) + originalFileExtension;
+						AttachFileEntity ataAttachFileEntity = new AttachFileEntity();
+						ataAttachFileEntity.setFileSize(multipartFile.getSize());
+						ataAttachFileEntity.setOriginalFileName(multipartFile.getOriginalFilename());
+						ataAttachFileEntity.setStoredFilePath(folderPath + "/" + newFileName);
+						ataAttachFileEntity.setCreatorId("admin");
+						fileList.add(ataAttachFileEntity);
+
+						file = new File(folderPath + "/" + newFileName);
+						multipartFile.transferTo(file);
+
+					} catch (Exception e) {
+						log.info("첨부 파일 파싱 에러 ");
+					}
+
 				}
 			}
 		}
